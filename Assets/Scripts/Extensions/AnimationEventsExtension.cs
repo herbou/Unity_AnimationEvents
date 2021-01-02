52cfb46a873360d978189a7db752f8d288cb8624
Hamza HERBOU <hamza95herbou@gmail.com>
2021-01-02 18:58:31 +0100

    added files

---
+++ Assets/Scripts/Extensions/AnimationEventsExtension.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine ;
+
+public static class AnimationEventsExtension {
+   /// <summary>
+   /// Add animation events to a clip at a given time.
+   /// </summary>
+   /// <param name="clip">Animation Clip.</param>
+   /// <param name="time">Time in Seconds.</param>
+   /// <param name="functionName">Function Name.</param>
+   public static void AddAnimationEvent (this AnimationClip clip, float time, string functionName) {
+      float clipDuration = clip.length ;
+
+      if (time < 0f) {
+         Debug.LogError ("Event time must be greater >= than <b><color=white>0.0</color>f</b> seconds") ;
+         return ;
+
+      } else if (time > clipDuration) {
+         Debug.LogError ("Event time must be less <= than the clip's duration: <b><color=white>" + clipDuration + "</color>f</b> seconds") ;
+         return ;
+      }
+
+      AnimationEvent animationEvent = new AnimationEvent {
+         time = time,
+         functionName = functionName
+      } ;
+
+      clip.AddEvent (animationEvent) ;
+   }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine ;
+
+public class Player : MonoBehaviour {
+
+   [SerializeField] private AnimationClip clip ;
+   [SerializeField] private float eventTime ;
+   
+
+   private void Start () {
+      clip.AddAnimationEvent (eventTime, "EventFunction") ;
+   }
+
+   private void EventFunction () {
+      Debug.Log ("Event fired") ;
+   }
+
+}
